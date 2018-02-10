9600fe978e63e1312c0852a1dc1a6d7e65237231
Charles Chamberlain <charlespipin@gmail.com>
2018-02-10 15:14:27 -0600

    Binary things

--- lib/creaturecoin.ex
+++ lib/creaturecoin.ex
@@ -1,22 +1,29 @@
 
-import IO
-
 defmodule Utils do
 
   def random_binary(length) do
     Enum.reduce(1..length, <<>>, fn(_, acc) -> acc <> <<:rand.uniform(127)>> end)
   end
 
+  def binary_to_list(<<>>) do
+    []
+  end
+
+  def binary_to_list(<<i :: size(8), rest :: binary>>) do
+    [i] ++ binary_to_list(rest)
+  end
+
 end
 
 defmodule Miner do
-  @zeros 5
+  @zeros 3
 
   def listen do
     receive do
       {:found_block, hash, block} ->
         # TODO: broadcast to rest of peoples
-        puts("found a block! " <> hash)
+        IO.puts("found a block! ")
+        IO.inspect(Utils.binary_to_list(hash))
     end
     listen()
   end
@@ -28,10 +35,11 @@ defmodule Miner do
       :extra_data => extra_data,
       :random_bits => Utils.random_binary(128),
     }
-    hash = Base.encode16(:crypto.hash(:sha256, Poison.encode!(block)))
+
+    hash = :crypto.hash(:sha256, Poison.encode!(block))
 
     # recurse unless we have a valid block
-    if String.slice(hash, 0..@zeros-1) == String.duplicate("0", @zeros) do
+    if String.slice(Base.encode16(hash), 0..@zeros-1) == String.duplicate("0", @zeros) do
       send(miner_pid, {:found_block, hash, block})
     else
       mine(miner_pid, owner, last_block, extra_data)
