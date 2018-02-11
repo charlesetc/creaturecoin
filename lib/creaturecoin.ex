ca22a1300623dbecad60ba31ed6f57a8d6908e68
Maia Conrado <amaiaconrado@gmail.com>
2018-02-10 22:07:33 -0600

    keys added

--- lib/creaturecoin.ex
+++ lib/creaturecoin.ex
@@ -5,8 +5,22 @@ defmodule Creaturecoin do
     Process.register ledger_pid, :ledger
     send ledger_pid, :initialize
 
+    if File.exists?("./keys/private_key.pem") do
+        private = File.read!("./keys/private_key.pem")
+    else
+        {:ok, private} = RsaEx.generate_private_key
+        File.write("./keys/private_key.pem", private)
+    end
+       
+    if File.exists?("./keys/public_key.pem") do
+        public = File.read!("./keys/public_key.pem")
+    else
+        {:ok, public} = RsaEx.generate_public_key(private)
+        File.write("./keys/public_key.pem", public)
+    end
+
     initial_state = %{
-      :owner_id => "Charles",
+      :owner_id => Base.encode16(public),
       :last_block => nil,
       :transactions => []
     }
