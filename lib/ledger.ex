0f7c8df4e7e40bb96515496951d4524b01c17ac8
Charles Chamberlain <charlespipin@gmail.com>
2018-02-11 00:12:38 -0600

    Storing ledger in file

--- lib/ledger.ex
+++ lib/ledger.ex
@@ -1,27 +1,34 @@
-
 defmodule Ledger do
 
   @binary_filename "ledger.binary"
+  @txt_filename "ledger.txt"
 
-  def loop(chain) do
+  def loop(chain, socket \\ nil) do
     receive do
       :initialize ->
+
         if File.exists?(@binary_filename) do
             new_chain = File.read!(@binary_filename) |> :erlang.binary_to_term
-            loop(new_chain)
+            loop(new_chain, socket)
         else
-            loop([])
+            loop([], socket)
         end
       {:new_block, block} ->
-        IO.inspect(chain |> Enum.map(fn {hash, block} -> Base.encode16(hash) end))
+        IO.inspect(chain |> Enum.map(fn {hash, _} -> Base.encode16(hash) end))
         new_chain = [block] ++ chain
         save(chain)
-        loop(new_chain)
+        loop(new_chain, socket)
     end
   end
 
   def save(chain) do
     File.write(@binary_filename, :erlang.term_to_binary chain)
+    File.write(
+      @txt_filename,
+      chain
+      |> Enum.map(fn {hash, _} -> Base.encode16(hash) end)
+      |> Enum.join("\n")
+    )
   end 
 
 end
